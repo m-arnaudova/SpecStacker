1b8703f6f8d0aefe7755bb9ff14421961a23f935
Marina Arnaudova <m.i.arnaudova@gmail.com>
2025-06-27 15:48:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='SpecStacker',  # Replace with your own package name
-    version='0.1.0',  # Initial release version
-    author='Marina Arnaudova',  # Replace with your name
-    author_email='m.i.arnaudova@example.com',  # Replace with your email
-    description='This is a new rest-frame spectral stacking code.',  # Short description of your package
+    version='1.0.0',  # Initial release version
+    author='Marina Arnaudova',  
+    author_email='m.i.arnaudova@gmail.com',  
+    description='This is a new rest-frame spectral stacking code.',  
     long_description=long_description,  # Long description read from the README file
-    long_description_content_type='text/markdown',  # Optional (see note above)
-    url='https://github.com/m-arnaudova/SpecStacker',  # Replace with the URL of your package
+    url='https://github.com/m-arnaudova/SpecStacker', 
     packages=find_packages(),  # Automatically find packages in the current directory
     classifiers=[
         'Programming Language :: Python :: 3',
